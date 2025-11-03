158ff9b4b1c544b2a43de0ec589a76e46604bf02
dave <newdevdave@gmail.com>
2025-11-03 13:34:14 -0500

    wtf am I doing

---
+++ src/componets/navbar.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Styles from "./navbar.module.css";
+// import Link from ".next/link";
+
+const Navbar = () => {
+    return (
+        <div class="homeNavBar">
+        </div>
+    )
+}
+
+export default Navbar
