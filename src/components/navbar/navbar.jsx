0c4664ea62c97c4d6855b647fb1fe6d279c11b5b
zhubenjamin <benjaminzhu2020@gmail.com>
2025-11-03 21:32:07 -0500

    add navbar styles

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,9 +9,18 @@ import Image from "next/image";
 const Navbar = () => {
 
     return (
-        <div>
-            <Image src="/saclogo.png" width="50" height="50" alt="saclogo"></Image>
-            <p>SAC</p>
+        <div className={styles.navbar}>
+            <div className={styles.navbar_left}>
+                <Image src="/saclogo.png" width="45" height="45" alt="sac logo"></Image>
+                <div className={styles.navbar_left_text}>
+                    <h1>SAC</h1>
+                </div>
+            </div>
+            <div className={styles.navbar_right}>
+                <p className={styles.navbar_right_element}>test</p>
+                <p className={styles.navbar_right_element}>test2</p>
+                <p className={styles.navbar_right_element}>test3</p>
+            </div>
         </div>
     )
 }
