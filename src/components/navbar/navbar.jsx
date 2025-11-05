6e3c1d24a9472f2642ee74eb305983a7c098ef6e
zhubenjamin <benjaminzhu2020@gmail.com>
2025-11-04 21:07:32 -0500

    change navbar to more closely align with figma design

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -13,13 +13,14 @@ const Navbar = () => {
             <div className={styles.navbar_left}>
                 <Image src="/saclogo.png" width="45" height="45" alt="sac logo"></Image>
                 <div className={styles.navbar_left_text}>
-                    <h1><a href="/">SAC</a></h1>
+                    <h1><a href="/">SAC RGSS</a></h1>
                 </div>
             </div>
             <div className={styles.navbar_right}>
-                <a className={styles.navbar_right_element} href="#">test</a>
-                <a className={styles.navbar_right_element} href="#">test2</a>
-                <a className={styles.navbar_right_element} href="#">test3</a>
+                {/* <a className={styles.navbar_right_element} href="#">Home</a> */}
+                <a className={styles.navbar_right_element} href="#">Clubs</a>
+                <a className={styles.navbar_right_element} href="#">Student Life</a>
+                <a className={styles.navbar_right_element} href="#">More Info</a>
             </div>
         </div>
     )
