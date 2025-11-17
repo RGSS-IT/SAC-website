5a65a31ba8be297334a9b54b3472a8b7957f02b6
tyler <349987800@gapps.yrdsb.ca>
2025-11-17 00:43:47 -0500

    Created ./clubs subdirectory and ClubDetails component

--- src/app/clubs/page.jsx
+++ src/app/clubs/page.jsx
@@ -1,1 +1,18 @@
+import Image from "next/image";
+import ClubDetails from "@/components/clubdetails/clubdetails";
+import styles from "./page.module.css";
 
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <h1>Clubs Page</h1>
+        <ClubDetails details={{
+          name:"HELLO",
+          banner_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png",
+          profile_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png",
+          room: "101",
+          frequency: "Weekly"
+        }}/>
+    </div>
+  );
+}
