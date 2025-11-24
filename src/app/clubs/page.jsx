a1e22e245b323aee2e5e9af04161dc815a58dd02
Tyler <349987800@gapps.yrdsb.ca>
2025-11-24 12:45:55 -0500

    club change update

--- src/app/clubs/page.jsx
+++ src/app/clubs/page.jsx
@@ -5,14 +5,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-        <h1>Clubs Page</h1>
+        <h1 className={styles.title}>GET INVOLVED</h1>
         <ClubDetails details={{
           name:"HELLO",
           banner_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png",
           profile_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png",
           room: "101",
           frequency: "Weekly"
-        }}/>
+        }}
+        className={styles.inter}/>
     </div>
   );
 }
